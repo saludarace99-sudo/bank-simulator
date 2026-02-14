529f27ebea335450374911d5068ba54a2e27df29
Montealto <saludarace99@gmail.com>
2026-02-14 10:35:12 +0800

    BankingView.cs

--- app/BankingView.cs
+++ app/BankingView.cs
@@ -1,12 +1,29 @@
-using ATMApp.Services;
-
-namespace ATMApp.View
+public static class BankingService
 {
-    public static class BankingView
+    public static double CheckBalance(double balance)
     {
-        public static void Run()
+        return balance;
+    }
+
+    public static void Deposit(ref double balance, double amount)
+    {
+        balance += amount;
+    }
+
+    public static void Withdraw(ref double balance, double amount, out bool success)
+    {
+        if (amount <= 0)
         {
-          
+            success = false;
+        }
+        else if (amount > balance)
+        {
+            success = false;
+        }
+        else
+        {
+            balance -= amount;
+            success = true;
         }
     }
 }
